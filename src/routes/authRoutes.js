7b386a3bd0bba1f7bb823a130aa1d06fed448327
Mac <mac@MACBOOKs-MacBook-Pro-2.local>
2020-03-12 15:39:18 +0530

    dwqdwqd

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -10,11 +10,43 @@ router.post('/signup',async(req,res)=>{
         await user.save()
         // const token=jwt.sign({userId:user._id},'SECRETE_KEY',{expiresIn:'1 hour'})
         // res.send({token});
-        return res.sendStatus(200).send('SignUp Successful :)')
+        return res.status(200).send('SignUp Successful :)')
     }
     catch(err)
     {
        return res.status(422).send(err.message)
     }
-})
+});
+router.post('/signin',async(req,res)=>{
+           
+    const {country_code,phone_number,password}=req.body;
+    if(!country_code || !phone_number || !password)
+    {
+        return res.status(422).send({
+            err:'Must provide email and password lavdu'
+        })
+    }
+    const user= await User.findOne({
+        phone_number:phone_number
+    });
+    console.log(user)
+    if(!user)
+    {
+        return res.status(422).send({
+            err:'phone not found'
+        })
+    }
+    try{
+        await user.comparePassword(password);
+        const token=jwt.sign({userId:user._id},'SECRETE_KEY',{expiresIn:'1 hour'})
+        res.send({token});
+    }
+    catch(err)
+    {
+        return res.status(422).send({
+            err:'Invalid Password or Email'
+        })
+    }
+    
+});
 module.exports=router;
