d34d88d9125e8fa5a8a8f433323426138579d04c
Mac <mac@MACBOOKs-MacBook-Pro-2.local>
2020-03-12 12:58:40 +0530

    qdwq

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,16 +4,16 @@ const jwt = require("jsonwebtoken");
 const User=mongoose.model('User')
 const router=express.Router();
 router.post('/signup',async(req,res)=>{
-    const {country_code,phone_number,password}=req.body;
+    const {email,password}=req.body;
     try{
-        const user=new User({country_code,phone_number,password});
+        const user=new User({email,password});
         await user.save()
-        const token=jwt.sign({userId:user._id},'SECRETE_KEY',{expiresIn:'1 hour'})
+        const token=jwt.sign({userId:user._id},'SECRETE_KEY')
         res.send({token});
     }
     catch(err)
     {
        return res.status(422).send(err.message)
     }
-})
+});
 module.exports=router;
