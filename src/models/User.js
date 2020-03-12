bb13fb058f712042d3ac4b71832615ecad108ec0
Mac <mac@MACBOOKs-MacBook-Pro-2.local>
2020-03-12 13:27:06 +0530

    qdqw

--- src/models/User.js
+++ src/models/User.js
@@ -17,25 +17,6 @@ const userSchema=new mongoose.Schema({
         required:true
     }
 });
-userSchema.pre('save',function(next){
-    const user=this;
-    if(!user.isModified('password')){
-        return next()
-    }
-    bcrypt.genSalt(10,(err,salt)=>{
-        if(err)
-        {
-            return next(err)
-        }
-        bcrypt.hash(user.password,salt,(err,hash)=>{
-            if(err)
-            {
-                return next(err)
-            }
-            user.password=hash;
-            next();
-        })
-    })
-});
+
 mongoose.model('User',userSchema);
 
