034d3f3391c62e105faad3438b08ff47875b7954
Mac <mac@MACBOOKs-MacBook-Pro-2.local>
2020-03-12 12:00:20 +0530

    wdqwd

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const PORT = process.env.PORT || 5000
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const mongoURI= 'mongodb+srv://admin:Sangram@8897@cluster0-paves.mongodb.net/test?retryWrites=true&w=majority'
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log("Database Connected Successfully!!!"))
+  .catch(err => console.log("Error while connecting to the database =>  " + err));
+
 express()
   .get('/', (req, res) => res.send('Hii there'))
   .listen(PORT, () => console.log(`Listening on a ${PORT}`));
