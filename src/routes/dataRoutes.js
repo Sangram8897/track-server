daa0a12b1c0e718b0eb9349a74855b235c57f755
Mac <mac@MACBOOKs-MacBook-Pro-2.local>
2020-03-12 16:12:46 +0530

    dsads

--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const User=mongoose.model('User')
 const router=express.Router();
+const requireAuth=require('../middleware/requireAuth');
 router.post('/info',async(req,res)=>{
     return res.status(200).send('Test Successful :)')
 });
